7cf68620fa680ba56dba75ee2197f168aa5dbea5
AstaPe <asta.petronaityte@gmail.com>
2024-06-03 22:07:45 +0300

    Update

--- project/classes/BankAccount.py
+++ project/classes/BankAccount.py
@@ -13,14 +13,19 @@ class BankAccount:
     def withdraw(self, amount):
         if amount <= self.balance:
             self.balance -= amount
+            print('Money withdrawn, your current balance:', self.balance)
             return True
         else:
+            print('Insufficient Funds. Available balance:', self.balance)
             return False
 
     def display_balance(self):
         return f"Balance: {self.balance:.2f}"
 
     def apply_for_loan(self, loan_amount, interest_rate, term, pledge):
+        if self.balance < 0:
+            print("Cannot apply for a loan with a negative balance.")
+            return None
         loan = Loan(loan_amount, interest_rate, term, pledge)
         self.loans.append(loan)
         return loan
--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from project.classes.BankSystem import BankSystem
+from project.classes.BankSystem import BankSystem, BankAccount
 
 if __name__ == "__main__":
     bank_system = BankSystem()
@@ -25,3 +25,54 @@ if __name__ == "__main__":
     # Display loans
     print(account_1.display_loans())
     print(account_2.display_loans())
+
+    # Display accounts
+    print("Accounts in the system:")
+    for account_info in bank_system.display_accounts():
+        print(account_info)
+
+    # Make deposits
+    account_1.deposit(0)
+    account_2.deposit(0)
+
+    account_1.withdraw(1000000)
+    account_2.withdraw(3500000)
+
+    # Display balances
+    print(f"Balance for Alice: {account_1.display_balance()}")
+    print(f"Balance for Bob: {account_2.display_balance()}")
+
+    # Apply for a loan
+    loan_1 = bank_system.apply_for_loan("Alice", 5000, 5, 10, "Car", 8000)
+    if loan_1:
+        print("Loan details for Alice:")
+        print(loan_1.display_loan_details())
+    else:
+        print("Failed to apply for loan for Alice")
+
+    # Apply for another loan
+    loan_2 = bank_system.apply_for_loan("Bob", 3000, 4.5, 5, "Bike", 2000)
+    if loan_2:
+        print("Loan details for Bob:")
+        print(loan_2.display_loan_details())
+    else:
+        print("Failed to apply for loan for Bob")
+
+    # Display loans for Alice
+    print("All loans for Alice:")
+    for loan_details in account_1.display_loans():
+        print(loan_details)
+
+    # Display loans for Bob
+    print("All loans for Bob:")
+    for loan_details in account_2.display_loans():
+        print(loan_details)
+
+    account_1.withdraw(10000)
+    account_2.withdraw(20000)
+
+    # Display balances
+    print(f"Balance for Alice: {account_1.display_balance()}")
+    print(f"Balance for Bob: {account_2.display_balance()}")
+
+
